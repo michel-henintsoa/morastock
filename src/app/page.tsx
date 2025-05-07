1eabe3288c52a5ef40c98fa3a0f1e5a8cfb45a34
michel-henintsoa <michelherilalaina@gmail.com>
2025-05-07 07:22:30 +0300

    card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,17 @@ export default function Home() {
     <>
       <header 
       className=
-        "after:bg-gray-900 after:w-full after:h-[3px] after:rounded-full after:absolute after:bottom-0 relative pb-3 w-full mt-2"
+        "after:bg-gray-900 after:w-full after:h-[3px] after:rounded-full after:absolute after:bottom-0 relative pb-2 w-full mt-2"
       >
-        <div id="icon" className="grid grid-cols-2 grid-rows-1 w-[150]">
-          <div className="w-[67] h-[60] bg-[#1D1C24] rounded-xl"></div>
-          <div className="w-[100] h-[60] font-inter align-bottom">
+        <div id="icon" className="grid grid-cols-2 grid-rows-1 w-[150] align-center">
+          <div className="w-[40] h-[40] bg-[#1D1C24] rounded"></div>
+          <div className="w-[100] h-[60] font-inter align-bottom font-bold">
             <h4>Mora</h4>
             <h4>Stokage</h4>
           </div>
+        </div>
+        <div>
+          
         </div>
       </header>
     </>
