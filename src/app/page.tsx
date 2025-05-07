e32f39c5e532cee71b91891fae4e6aef98bca439
michel-henintsoa <michelherilalaina@gmail.com>
2025-05-07 08:51:48 +0300

    commenting and formating code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,9 +22,12 @@ export default function Home() {
     <>
       <header 
       className=
-        "after:w-full after:h-[1px] after:rounded-full after:absolute after:bottom-0 after:bg-zinc-500 relative pb-1 w-full mt-2 flex justify-between"
+        {`
+          after:w-full after:h-[1px] after:rounded-full after:absolute after:bottom-0 after:bg-zinc-500
+          relative pb-1 w-full mt-2 flex justify-between
+          `}
       >
-        
+        {/* Logo */}
         <div id="icon" className="grid grid-cols-2 grid-rows-1 w-[150] align-center">
           <div className="w-[40] h-[40] bg-[#1D1C24] rounded"></div>
           <div className="w-[100] h-[60] font-inter align-bottom font-bold">
@@ -32,6 +35,7 @@ export default function Home() {
             <h4>Stokage</h4>
           </div>
         </div>
+        {/* Navigation Items */}
         <div className="flex items-end space-x-2">
           {navigationItems.map((item) => (
             <button
